a6f1d4bcea383bc82bdbda341a88595510ba5a0f
Andrew Philpot <andrew.philpot@gmail.com>
2015-03-17 11:38:00 -0700

    amber update

--- sample/feature/eyecolor/eyeamber/fetch.sh
+++ sample/feature/eyecolor/eyeamber/fetch.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-curl -s -k -u 'darpamemex:darpamemex' -o output.json -d @query.json -XGET 'https://esc.memexproxy.com/dig-ht-pilot-unfiltered/_search'
+curl -s -k -u 'darpamemex:darpamemex' -o output.json -d @query.json -XGET 'https://esc.memexproxy.com/dig-ht-pilot-unfiltered03/_search'
