846de274825723b7232e50ade6cac4b1b6997d15
Andrew Philpot <andrew.philpot@gmail.com>
2015-04-03 16:27:30 -0700

    script only

--- sample/feature/multicategory_pilot/multicategory_pilot.sh
+++ sample/feature/multicategory_pilot/multicategory_pilot.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
+# 3 April 2015
+
 curl -k -o multicategory_pilot.json -XGET 'https://darpamemex:darpamemex@esc.memexproxy.com/dig-ht-pilot-test03/_search' -d '{"fields": ["hasBodyPart.text.english"], "size": 200, "query": {"function_score" : {"query" : { "match_all": {} }, "random_score" : {}    }}}'
