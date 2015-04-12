00cdc1bd9d50cd5922990946d27c9d173152d8d4
Andrew Philpot <andrew.philpot@gmail.com>
2015-04-12 10:45:24 -0700

    default instructions file page.html to directory from format file

--- sample/create_hit_configs.py
+++ sample/create_hit_configs.py
@@ -1,6 +1,16 @@
 #!/usr/bin/python
 
 # 4 March 2015
+# 10 April 2015
+
+# Adapted from window.py
+# drop many options
+# drop ahead, behind
+# drop textConditioner
+# jobname becomes -j/--experiment
+# drop ratio, shuffle, skip, SEED
+# drop/stub matcher
+# get rid of USEDOCID
 
 import sys, os
 try:
@@ -13,8 +23,6 @@ import random
 import nltk
 import itertools
 import argparse
-import pprint
-import hashlib
 import uuid
 from htmltoken import tokenize, bucketize
 
@@ -29,12 +37,7 @@ import cgi
 import tempfile
 
 import util
-from util import echo
-
-SEED = 20150304
-random.seed(SEED)
-
-
+from util import echo, ensureDirectoriesExist
 
 def truth(*args):
     return True
@@ -158,7 +161,7 @@ FORMAT = """=[
   }}
 ]"""
 
-def generateSentencesJson(jobname, output):
+def generateSentencesJson(experiment, output):
     sentences = []
     for d in output:
         sentences.append({"id": d["id"],
@@ -206,17 +209,13 @@ def intOrNone(thing):
     else:
         return int(thing)
 
-AHEAD=None
-BEHIND=None
-RATIO=0.5
 # MATCHER="containsEye"
 MATCHER=lambda x: True
 GENERATOR="genescaped"
-TEXTCONDITIONER=None
 CHECK=standardCheck
 SKIP=None
 
-def generateMatchContexts(words, behind, ahead, matcher):
+def generateMatchContexts(words, behind, ahead, matcher=MATCHER):
     if ahead and behind:
         # multiple matches
         for (word, i) in itertools.izip(words, itertools.count()):
@@ -231,19 +230,15 @@ def generateMatchContexts(words, behind, ahead, matcher):
     else:
         return
 
-USEDOCID = True
 BEGIN_COMMENT = """<!-- ##begin## -->"""
 END_COMMENT = """<!-- ##end## -->"""
 
 seen = {}
-@echo
-def windows(elsjson, ratio=0.9, matcher=containsEye, textConditioner=None, generator=genescaped, ahead=5, behind=5, 
-            limit=5, write=False, format=None, instructions=None, jobname=None,
-            field="hasBodyPart.text", shuffle=None, seen=seen, cloud=False, check=CHECK, skip=SKIP, 
-            verbose=False):
+def create_hit_configs(elsjson, generator=genescaped,
+                       limit=10, hitcount=10, write=False, format=None, instructions=None, experiment=None,
+                       field="hasBodyPart.text", seen=seen, cloud=False, check=CHECK, skip=SKIP, 
+                       verbose=False):
 
-    matcher, matcherName = interpretFnSpec(matcher)
-    textConditioner, textConditionerName = interpretFnSpec(textConditioner if textConditioner else None)
     generator, generatorName = interpretFnSpec(generator)
     check, checkName = interpretFnSpec(check)
     if format:
@@ -255,123 +250,117 @@ def windows(elsjson, ratio=0.9, matcher=containsEye, textConditioner=None, gener
             p1 = instructions.index(BEGIN_COMMENT)
             p2 = instructions.index(END_COMMENT)+len(END_COMMENT)
             instructions = json.dumps(instructions[p1:p2])
-    if not jobname:
-        jobname = str(uuid.uuid4())
-    if shuffle==None:
-        # unspecified, so default
-        # pass shuffle=False if you are sure you want to turn if off
-        shuffle = ratio>0
+    if not experiment:
+        experiment = str(uuid.uuid4())
 
     output = []
     with open(elsjson, 'r') as f:
         input = json.load(f)
     ehits = input["hits"]["hits"]
-    if shuffle:
-        random.shuffle(ehits)
     uid = None
 
-    def nested(limit):
-        for ehit in ehits:
-            docId = ehit["_id"]
-            docIndex = ehit["_index"]
-            fields = ehit.get("fields")
+    def publish_hit(experiment, hitCount, records):
+        if write:
+            data = generateSentencesJson(experiment, output)
+            outpath = 'config/%s__%04d.json' % (experiment, hitCount)
+            sio = StringIO.StringIO()
+            sio.write(format.format(sentences=data,instructions=instructions))
+            jdata = sio.getvalue()
+            # Dump the intermediate (post-substitution) JSON
             if verbose:
-                print >> sys.stderr, "Ehit: %r" % (ehit)
-            payloads = fields and fields.get(field, [])
-            if verbose:
-                print >> sys.stderr, "Payloads for field %r: %r" % (field, payloads)
-            problem = None
-            if payloads:
-                for payload in ehit["fields"][field]:
-                    if seen.get(payload, False):
-                        # already seen this one
-                        continue
-                    payload = textConditioner(payload) if textConditioner else payload
-                    problem = check(payload)
-                    if problem:
-                        if verbose:
-                            print >> sys.stderr, "broken/rejected row [%s] %r" % (problem, ehit)                  
-                        continue
-                    if random.random() > ratio:
+                jfile = os.path.join(tempfile.gettempdir(), "jdata__%s__%04d.json" % (experiment, hitCount))
+                with open(jfile, 'w') as f:
+                    print >> f, jdata
+            # Validate the JSON
+            try:
+                json.loads(jdata)
+            except Exception as e:
+                print >> sys.stderr, "Invalid JSON [%r]" % e
+            if cloud:
+                c = boto.connect_s3(profile_name=PROFILE_NAME)
+                b = c.get_bucket(BUCKETNAME)
+                expName = 'ner/%s' % (experiment)
+                keyName = 'ner/%s/%s' % (experiment, outpath)
+                k = b.new_key(keyName)
+                k.set_contents_from_string(jdata)
+                k.set_canned_acl('public-read')
+                return "https://s3-us-west-2.amazonaws.com/aisoftwareresearch/ner/%s" % experiment
+            else:
+                outfile = os.path.join(tempfile.gettempdir(), outpath)
+                ensureDirectoriesExist(outfile)
+                with open(outfile, 'w') as f:
+                    f.write(jdata)
+                return outpath
+        else:
+            print >> sys.stderr, "Would write %s %s with %s records" % (experiment, hitCount, len(records))
+            return output
+
+
+    # we want to generate HITCOUNT files
+    # each with LIMIT
+
+    def nested(hitcount, limit):
+        hitNum = 0
+        while hitNum < hitcount:
+            output = []
+            sentenceNum = 0
+            while len(output)<limit:
+                ehit = ehits.pop(0)
+                docId = ehit["_id"]
+                docIndex = ehit["_index"]
+                fields = ehit.get("fields")
+                if verbose:
+                    print >> sys.stderr, "Ehit: %r" % (ehit)
+                payloads = fields and fields.get(field, [])
+                if verbose:
+                    print >> sys.stderr, "Payloads for field %r: %r" % (field, payloads)
+                problem = None
+                if payloads:
+                    for payload in ehit["fields"][field]:
+                        if seen.get(payload, False):
+                            # already seen this one
+                            continue
+                        problem = check(payload)
+                        if problem:
+                            if verbose:
+                                print >> sys.stderr, "broken/rejected row [%s] %r" % (problem, ehit)                  
+                            continue
                         if verbose:
                             print >> sys.stderr, "processing %s" % docId
                         # we are interested in this instance
                         words = [word for word in generator(payload)]
                         # all matches or just one match:
-                        for (start, end) in generateMatchContexts(words, behind, ahead, matcher):
-                            skip = None
-                            if skip:
-                                skip -=1
-                                if skip > 0:
-                                    continue
+                        for (start, end) in generateMatchContexts(words, None, None, MATCHER):
                             output.append({"X-indexId": docId, 
                                            "X-indexName": docIndex,
                                            "X-field": field,
-                                           "X-matchAnchor": matcherName,
-                                           "X-textConditioner": textConditionerName,
                                            "X-generator": generatorName,
                                            "X-reqWindowWidth": (end-start)+1,
                                            "X-tokenStart": start,
                                            "X-tokenEnd": end,
                                            "X-elasticsearchJsonPathname": elsjson,
-                                           "id": docId if USEDOCID else hashlib.sha1("%s-%s-%s" % (docId, start, end)).hexdigest(),
+                                           "id": docId,
                                            "tokens": words[start:end]
-                                           # "markup": " ".join(["<span>%s</span>" % word for word in words])
                                            })
-                            if limit:
-                                limit -= 1
-                                if limit <= 0:
-                                    return output
-            else:
-                if verbose:
-                    print >> sys.stderr, "No payloads for ehit %r" % (ehit)
-
-    nested(limit)
-    if write:
-        data = generateSentencesJson(jobname, output)
-        outfile = 'config/%s.json' % jobname
-        sio = StringIO.StringIO()
-        sio.write(format.format(sentences=data,instructions=instructions))
-        jdata = sio.getvalue()
-        # Dump the intermediate (post-substitution) JSON
-        if verbose:
-            jfile = os.path.join(tempfile.gettempdir(), "jdata.json")
-            with open(jfile, 'w') as f:
-                print >> f, jdata
-        # Validate the JSON
-        try:
-            json.loads(jdata)
-        except Exception as e:
-            print >> sys.stderr, "Invalid JSON [%r]" % e
-        if cloud:
-            c = boto.connect_s3(profile_name=PROFILE_NAME)
-            b = c.get_bucket(BUCKETNAME)
-            keyName = 'ner/%s/%s' % (jobname, outfile)
-            k = b.new_key(keyName)
-            k.set_contents_from_string(jdata)
-            k.set_canned_acl('public-read')
-            return "https://s3-us-west-2.amazonaws.com/aisoftwareresearch/%s" % keyName
-        else:
-            with open(outfile, 'w') as f:
-                f.write(jdata)
-            return outfile
-    else:
-        return output
+                else:
+                    if verbose:
+                        print >> sys.stderr, "No payloads for ehit %r" % (ehit)
+            # this hit is fully populated
+            publish_hit(experiment, hitNum, output)
+            output = []
+            hitNum += 1
+    nested(hitcount, limit)
 
 def main(argv=None):
     '''this is called if run from command line'''
     parser = argparse.ArgumentParser()
     parser.add_argument("elsjson", help='input json file', type=lambda x: isValidFileArg(parser, x))
-    parser.add_argument('-r','--ratio', required=False, help='ratio of accepts', default=RATIO, type=float)
-    parser.add_argument('-m','--matcher', required=False, default=MATCHER, type=str)
-    parser.add_argument('-t','--textConditioner', required=False, default=TEXTCONDITIONER, type=str)
     parser.add_argument('-g','--generator', required=False, default=GENERATOR, type=str)
-    parser.add_argument('-a','--ahead','--after', required=False, default=AHEAD, type=intOrNone)
-    parser.add_argument('-b','--behind','--before', required=False, default=BEHIND, type=intOrNone)
     parser.add_argument('-l','--limit', required=False, default=None, type=int)
+    parser.add_argument('-k','--hitcount', required=False, default=None, type=int)
     parser.add_argument('-w','--write', required=False, action='store_true')
     parser.add_argument('-f','--format', required=False, help='format template', type=lambda x: isValidFileArg(parser, x))
-    parser.add_argument('-j','--jobname', required=False, help='jobname', type=str, default=None)
+    parser.add_argument('-j','--experiment', required=False, help='experiment', type=str, default=None)
     parser.add_argument('-c','--cloud', required=False, help='cloud', action='store_true')
     parser.add_argument('-e','--field', required=False, help='field', type=str, default="hasBodyPart.text.english")
     parser.add_argument('-x','--check', required=False, help='check', type=str, default=CHECK)
@@ -380,26 +369,27 @@ def main(argv=None):
     args=parser.parse_args()
 
     elsjson = args.elsjson
-    ratio = args.ratio
-    matcher = args.matcher
-    textConditioner = args.textConditioner if args.textConditioner else None
     generator = args.generator
-    ahead = args.ahead
-    behind = args.behind
     limit = args.limit
+    hitcount = args.hitcount
     write = args.write
     format = args.format
     instructions = '/Users/philpot/Documents/project/dig-mturk/sample/pyfmt/embed/page.html'
-    jobname = args.jobname
+    print >> sys.stderr, "format %r instructions %r" % (format, instructions)
+    (dirpath, _) = os.path.split(format)
+    instructions = os.path.join(dirpath, 'page.html')
+    print >> sys.stderr, "format %r instructions %r" % (format, instructions)
+    experiment = args.experiment
     cloud = args.cloud
     field = args.field
     check = args.check
     skip = None if args.skip==0 else args.skip
     verbose = args.verbose
-    s = windows(elsjson, ratio=ratio, matcher=matcher, textConditioner=textConditioner, generator=generator, 
-                ahead=ahead, behind=behind, limit=limit, write=write, format=format, instructions=instructions,
-                jobname=jobname, 
-                cloud=cloud, field=field, check=check, skip=skip, verbose=verbose)
+    s = create_hit_configs(elsjson, experiment=experiment, generator=generator, 
+                           write=write, cloud=cloud, 
+                           format=format, instructions=instructions,
+                           field=field, check=check, limit=limit, hitcount=hitcount, skip=skip, 
+                           verbose=verbose)
 
 # call main() if this is run as standalone
 if __name__ == "__main__":
