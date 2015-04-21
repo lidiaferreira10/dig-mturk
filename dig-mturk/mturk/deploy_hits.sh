e3330f313e781ecbd44095195be5d64baa6bf227
Andrew Philpot <andrew.philpot@gmail.com>
2015-04-21 15:41:14 -0700

    now has 5 args

--- dig-mturk/mturk/deploy_hits.sh
+++ dig-mturk/mturk/deploy_hits.sh
@@ -2,5 +2,8 @@
 
 STAGING_AREA=$1
 EXPERIMENT=$2
+PROFILE_NAME=$3
+AWS_KEY=$4
+AWS_SECRET=$5
 
-mvn exec:java -Dexec.mainClass="edu.isi.dig.mturk.hitFiles" -Dexec.args="$STAGING_AREA $EXPERIMENT"
+mvn exec:java -Dexec.mainClass="edu.isi.dig.mturk.hitFiles" -Dexec.args="$STAGING_AREA $EXPERIMENT $PROFILE_NAME $AWS_KEY $AWS_SECRET"
