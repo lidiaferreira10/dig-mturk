6d953e05f9824c645c19927e4f04b56ec3573573
Andrew Philpot <andrew.philpot@gmail.com>
2015-04-28 10:11:23 -0700

    construction of sentenceUri a little more bulletproof

--- data-processing/karma/python/mturk.py
+++ data-processing/karma/python/mturk.py
@@ -1,7 +1,24 @@
 
 
-def mtSentenceUri(docUri):
-    return docUri+"/sentence"
+def mtSentenceUri(sentenceId):
+    sid = getValue("sentenceId")
+    if sid=="emptyanswer":
+        return ""
+    elif sid.startswith("http"):
+        return sid
+    else:
+        return "http://dig.isi.edu/sentence/" + sid
 
 def mtAnnotationSetUri(sentenceUri):
-	return sentenceUri+"/annotationset"
+    return sentenceUri+"/annotationset"
+
+categoryToAnnotationClass = {"Person name": "personName",
+                             "Ethnicity/Origin": "ethnicityOrigin",
+                             "Hair type": "hairType",
+                             "Eye color": "eyeColor",
+                             "Company name": "companyName"}
+
+def mtAnnotationClass(category):
+    return categoryToAnnotationClass.get(category, "")
+
+    
