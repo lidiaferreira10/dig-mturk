b04f80bbd1250dc0ef13b83e9ded99597dc2460a
Andrew Philpot <andrew.philpot@gmail.com>
2015-04-30 16:21:11 -0700

    use default_operator, sticking OR in the query really blows things up

--- sample/feature/eye-hair/eye-hair.sh
+++ sample/feature/eye-hair/eye-hair.sh
@@ -5,9 +5,8 @@ SIZE=${1:-200}
 curl -k -o eye-hair_${SIZE}.json -XGET 'https://darpamemex:darpamemex@esc.memexproxy.com/dig-latest/_search' -d '{"fields": ["hasBodyPart.text.english"], 
   "query": {
     "function_score" : {
-      "query": {"simple_query_string": {"query": "eye~ OR blue~ OR brown~ OR green~ OR hazel~ OR amber~ OR gray~ OR grey~ OR hair~ OR black~ OR blonde~ OR red~ OR auburn~ OR chestnut~ OR silver~ OR ginger~ OR locks~ OR curls~ OR tresses~"}},
+      "query": {"simple_query_string": {"query": "eye~ blue green brown hazel hair blond~ brunette chestnut ginger",
+                                        "default_operator": "OR"}},
       "random_score" : {}
     }
 }, "size": 200 }'
-
-
