2f6970ba855536e85d7741636ecd45009c97b450
Andrew Philpot <andrew.philpot@gmail.com>
2015-05-01 14:46:29 -0700

    seems to fix thread cleanup issues

--- dig-mturk/mturk/deploy_hits.sh
+++ dig-mturk/mturk/deploy_hits.sh
@@ -6,4 +6,4 @@ PROFILE_NAME=$3
 AWS_KEY=$4
 AWS_SECRET=$5
 
-mvn exec:java -Dexec.mainClass="edu.isi.dig.mturk.hitFiles" -Dexec.args="$STAGING_AREA $EXPERIMENT $PROFILE_NAME $AWS_KEY $AWS_SECRET"
+mvn exec:java -Dexec.mainClass="edu.isi.dig.mturk.hitFiles" -Dexec.cleanupDaemonThreads="false" -Dexec.args="$STAGING_AREA $EXPERIMENT $PROFILE_NAME $AWS_KEY $AWS_SECRET"
