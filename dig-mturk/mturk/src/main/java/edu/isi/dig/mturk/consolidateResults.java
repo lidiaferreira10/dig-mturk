068f75a3ad886f046d5be36dd6c5725793ed0573
Andrew Philpot <andrew.philpot@gmail.com>
2015-05-07 13:46:50 -0700

    typo in header

--- dig-mturk/mturk/src/main/java/edu/isi/dig/mturk/consolidateResults.java
+++ dig-mturk/mturk/src/main/java/edu/isi/dig/mturk/consolidateResults.java
@@ -41,7 +41,7 @@ public class consolidateResults {
 				 * that the files required for a particular hit aren't mixed up
 				 * with data from another hit instance.
 				 */
-				result = "HitId\tWorkerId\tAssignmentId\tSentenceId\tOffset\tHighlightedText\tCategory\tText\tEncodedTokens\tTokenIds\n";
+				result = "HitId\tWorkerId\tAssignmentId\tSentenceId\tOffset\tHighlightedText\tCategory\tText\tEncodedTokens\tTokenIdxs\n";
 				for (S3ObjectSummary objectSummary : objectListing
 						.getObjectSummaries()) {
 					String key = objectSummary.getKey();
