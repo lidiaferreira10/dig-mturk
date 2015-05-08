5323a6c65711c28f4160f5a4f9d1e2bc2dabd351
Andrew Philpot <andrew.philpot@gmail.com>
2015-05-07 17:27:23 -0700

    routines for processing tabsep cell data for export

--- data-processing/karma/python/mturk.py
+++ data-processing/karma/python/mturk.py
@@ -26,16 +26,24 @@ def mtAnnotationClass(category):
 safeSeparatorCharacter = "\t"
 
 def mtDecodeTokens(encodedTokens):
-    return safeSeparatorCharacter.join(json.loads(base64.b64decode(encodedTokens)))
+    # return safeSeparatorCharacter.join(json.loads(base64.b64decode(encodedTokens)))
+    return base64.b64decode(encodedTokens)
 
-def mtExtractAnnotatedTokens(tokenIds, joinedTokens):
-    """Beware of fencepost error here"""
-    ids = [int(i) for i in tokenIds.split(',') if (i and i>0)]
+def validTokenIdx(thing):
+    "any inteeger >= 0; remove -1, empty strings"
+    try:
+        return int(thing)>=0
+    except:
+        pass
+    return False
+
+def mtExtractAnnotatedTokens(tokenIdxs, joinedTokens):
+    idxs = [int(i) for i in tokenIdxs.split(',') if validTokenIdx(i)]
     tokens = joinedTokens.split(safeSeparatorCharacter)
-    annotated = [tokens[i] for i in ids]
+    annotated = [tokens[i] for i in idxs]
     return safeSeparatorCharacter.join(annotated)
 
-
-def mtDecodeTokens2(encodedTokens):
-    return json.loads("[1, 2]")
+def mtExtractAnnotatedTokenIdxs(tokenIdxs):
+    idxs = [i for i in tokenIdxs.split(',') if validTokenIdx(i)]
+    return safeSeparatorCharacter.join(idxs)
 
