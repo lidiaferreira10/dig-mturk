948e2a91aa9c025f762a6f8c47d0c50efa5d037d
Andrew Philpot <andrew.philpot@gmail.com>
2015-06-15 13:28:52 -0700

    10k sentences, pretty printed

--- sample/feature/eyehair/eyehair.sh
+++ sample/feature/eyehair/eyehair.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-SIZE=${1:-2000}
+SIZE=${1:-10000}
 
-curl -k -o eyehair_${SIZE}.json -XGET 'https://darpamemex:darpamemex@esc.memexproxy.com/dig-latest/_search' -d '{"fields": ["hasBodyPart.text.english"], 
+curl -k -XGET 'https://darpamemex:darpamemex@esc.memexproxy.com/dig-latest/_search' -d '{"fields": ["hasBodyPart.text.english"], 
   "query": {
     "function_score" : {
       "query": {"simple_query_string": {"query": "eye~ blue green brown hazel hair blond~ brunette chestnut ginger redhead",
                                         "default_operator": "OR"}},
       "random_score" : {}
     }
-}, "size": 2000 }'
+}, "size": 10000 }' | jq "." > eyehair_${SIZE}.json 
